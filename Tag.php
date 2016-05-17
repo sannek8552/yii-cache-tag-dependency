e9e250095da68c0ce0910fced6f5d18752db52a5
Dlinny <anton.zm@gmail.com>
2016-05-17 11:37:33 +0100

    Use tagCache if exists instead of global cache

--- Tag.php
+++ Tag.php
@@ -8,84 +8,92 @@ namespace TaggedCache;
  */
 class Tag
 {
-	private $_tag;
+    private $_tag;
 
-	private static $_values = array();
+    private static $_values = array();
 
-	public function __construct($tag = null)
-	{
-		$this->setTag($tag);
-	}
+    public function __construct($tag = null)
+    {
+        $this->setTag($tag);
+    }
 
-	/**
-	 * Set name of tag
-	 * @param mixed $this
-	 */
-	public function setTag($tag = null)
-	{
-		if (empty($tag))
-		{
-			$tag = '@' . get_class($this);
-		}
-		if ('@' !== $tag{0})
-		{
-			$tag = '@' . get_class($this) . '::' . $tag;
-		}
-		$this->_tag = $tag;
-		return $this;
-	}
+    /**
+     * Set name of tag
+     * @param mixed $tag
+     * @return Tag
+     */
+    public function setTag($tag = null)
+    {
+        if (empty($tag)) {
+            $tag = '@' . get_class($this);
+        }
+        if ('@' !== $tag{0}) {
+            $tag = '@' . get_class($this) . '::' . $tag;
+        }
+        $this->_tag = $tag;
+        return $this;
+    }
 
-	/**
-	 * Return name of tag
-	 * @return mixed
-	 */
-	public function getTag()
-	{
-		return $this->_tag;
-	}
+    /**
+     * Return name of tag
+     * @return mixed
+     */
+    public function getTag()
+    {
+        return $this->_tag;
+    }
 
-	/**
-	 * Return current version of tag
-	 * @return mixed
-	 */
-	public function get()
-	{
-		if (!isset(self::$_values[$this->getTag()]))
-			return false;
+    /**
+     * Return current version of tag
+     * @return mixed
+     */
+    public function get()
+    {
+        if (!isset(self::$_values[$this->getTag()]))
+            return false;
 
-		return self::$_values[$this->getTag()];
-	}
+        return self::$_values[$this->getTag()];
+    }
 
-	/**
-	 * Set current tag version
-	 * @param mixed $value
-	 * @return $this
-	 */
-	public function set($value)
-	{
-		self::$_values[$this->getTag()] = $value;
-		return $this;
-	}
+    /**
+     * Set current tag version
+     * @param mixed $value
+     * @return $this
+     */
+    public function set($value)
+    {
+        self::$_values[$this->getTag()] = $value;
+        return $this;
+    }
 
-	/**
-	 * Save current tag version into cache
-	 * @return $this
-	 */
-	public function save()
-	{
-		if (false !== ($value = $this->get()))
-			\Yii::app()->cache->set($this->getTag(), $value, /* forever */ 0);
-		return $this;
-	}
+    /**
+     * Save current tag version into cache
+     * @return $this
+     */
+    public function save()
+    {
+        if (false !== ($value = $this->get())) {
+            $this->cache()->set($this->getTag(), $value, 0/* forever */);
+        }
+        return $this;
+    }
 
-	/**
-	 * Remove tag value from cache
-	 * @return $this
-	 */
-	public function delete()
-	{
-		unset(self::$_values[$this->getTag()]);
-		\Yii::app()->cache->delete($this->getTag());
-		return $this;
-	}
+    /**
+     * Remove tag value from cache
+     * @return $this
+     */
+    public function delete()
+    {
+        unset(self::$_values[$this->getTag()]);
+        \Yii::app()->cache->delete($this->getTag());
+        return $this;
+    }
+
+    /**
+     * @return \CCache
+     */
+    private function cache()
+    {
+        return \Yii::app()->hasComponent('tagCache') ? \Yii::app()->tagCache : \Yii::app()->cache;
+    }
 }
